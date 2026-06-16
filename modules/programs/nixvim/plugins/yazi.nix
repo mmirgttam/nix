0f9619a100eedce2d2c40734ffab37e6f89fa7c8
Matt Grimm <matt.t.grimm@gmail.com>
2026-06-16 19:28:45 +0100

    nixvim: open dirs with yazi

--- modules/programs/nixvim/plugins/yazi.nix
+++ modules/programs/nixvim/plugins/yazi.nix
@@ -1,9 +1,15 @@
 {
+  globals.loaded_netrwPlugin = 1;
+
   plugins.yazi = {
     enable = true;
-    settings.integrations = {
-      grep_in_directory = "fzf-lua";
-      grep_in_selected_files = "fzf-lua";
+    settings = {
+      open_for_directories = true;
+
+      integrations = {
+        grep_in_directory = "fzf-lua";
+        grep_in_selected_files = "fzf-lua";
+      };
     };
   };
 }
