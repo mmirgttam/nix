f3869fe5da2c2eb5ac3c80049b67b59f208522b1
mmirgttam <matt.t.grimm@gmail.com>
2026-06-16 14:42:04 +0100

    set some nice qol system prefs on macos

--- modules/platforms/aarch64-darwin.nix
+++ modules/platforms/aarch64-darwin.nix
@@ -1,7 +1,34 @@
-{ lib, ... }: {
+{ lib, ... }:
+let
+  # Key mapping overrides.
+  # Use https://hidutil-generator.netlify.app to find key codes.
+  keyMappings = builtins.toJSON {
+    UserKeyMapping = [
+      # Caps Lock -> Escape
+      {
+        HIDKeyboardModifierMappingSrc = 30064771129; # 0x700000039
+        HIDKeyboardModifierMappingDst = 30064771113; # 0x700000029
+      }
+    ];
+  };
+in {
   home.sessionVariables = {
     WEZTERM_SHELL_SKIP_USER_VARS = "1";
     WEZTERM_SHELL_SKIP_CWD = "1";
+  };
+
+  targets.darwin.defaults."com.apple.dock".autohide = true;
+  targets.darwin.defaults."com.apple.menuextra.clock".Show24Hour = true;
+
+  # Apply immediately when Home Manager switches, and again at login, since this
+  # is not preserved across reboots.
+  home.activation.keyMappings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    /usr/bin/hidutil property --set '${keyMappings}' || true
+  '';
+
+  launchd.agents.keyboard-mapping.config = {
+    ProgramArguments = [ "/usr/bin/hidutil" "property" "--set" keyMappings ];
+    RunAtLoad = true;
   };
 
   matt.home-manager.switchConfiguration = lib.mkDefault "matt-aarch64-darwin";
