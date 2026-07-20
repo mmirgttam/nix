430969ba857e994937c3521571ee8300340bc618
Matt Grimm <matt.t.grimm@gmail.com>
2026-07-20 09:49:15 +0100

    nixvim: follow nixpkgs to prevent infinite recursion issue

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     workmux.url = "github:raine/workmux";
   };
