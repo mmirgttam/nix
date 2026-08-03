d5c745c26bd37f9f17dd2e81fccc754c09eb412a
Matt Grimm <matt.t.grimm@gmail.com>
2026-08-03 13:41:58 +0100

    firefox: drop it

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -4,15 +4,9 @@
     ../programs/wezterm
   ];
 
-  #programs.firefox.enable = true;
-
   home.packages = with pkgs; [
     signal-desktop
   ] ++ lib.optionals pkgs.stdenv.isLinux [
     gimp
-  ] ++ lib.optionals pkgs.stdenv.isDarwin [
-    #(pkgs.writeShellScriptBin "firefox" ''
-    #  exec "$HOME/Applications/Home Manager Apps/Firefox.app/Contents/MacOS/firefox" "$@"
-    #'')
   ];
 }
