1b79fe444bf81de81d9c43929db16a07525c3947
Matt Grimm <matt.t.grimm@gmail.com>
2026-08-06 07:58:22 +0100

    fix ruff breakage

--- modules/profiles/python-dev.nix
+++ modules/profiles/python-dev.nix
@@ -1,19 +1,21 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     (python313.withPackages (ps: [
-      ps.python-lsp-ruff
       ps.python-lsp-server
     ]))
     uv
   ];
 
-  programs.nixvim.plugins.lsp.servers.pylsp = {
-    enable = true;
-    settings.plugins = {
-      ruff.enabled = true;
-      pycodestyle.enabled = false;
-      pyflakes.enabled = false;
-      yapf.enabled = false;
+  programs.nixvim.plugins.lsp.servers = {
+    pylsp = {
+      enable = true;
+      settings.plugins = {
+        pycodestyle.enabled = false;
+        pyflakes.enabled = false;
+        yapf.enabled = false;
+      };
     };
+
+    ruff.enable = true;
   };
 }
